45fd0750db068c45c07bc6bbe2462871eca16b17
Чинаев <ax.chinaev@yandex.ru>
2022-02-10 23:33:10 +0300

    Исправленная обратная матрица

--- project/src/matrix.cpp
+++ project/src/matrix.cpp
@@ -5,11 +5,11 @@
 #include "exceptions.h"
 
 namespace prep {
-    Matrix::Matrix(size_t rows, size_t cols): rows(rows), cols(cols), matrix(rows, std::vector<double> (cols, 0)) {}
+    Matrix::Matrix(size_t rows, size_t cols) : rows(rows), cols(cols), matrix(rows, std::vector<double>(cols, 0)) {}
 
-    Matrix::Matrix(std::istream& is) {
+    Matrix::Matrix(std::istream &is) {
         is >> rows >> cols;
-        if (is.eof()) {throw InvalidMatrixStream();}
+        if (is.eof()) { throw InvalidMatrixStream(); }
         size_t elements_count = 0;
         size_t row = 0;
         size_t col = 0;
@@ -23,28 +23,28 @@ namespace prep {
             ++col;
             ++elements_count;
         }
-        if (is.fail() || (rows * cols != elements_count)) {throw InvalidMatrixStream();}
+        if (is.fail() || (rows * cols != elements_count)) { throw InvalidMatrixStream(); }
     }
 
-    size_t Matrix::getRows() const{
+    size_t Matrix::getRows() const {
         return rows;
     }
 
-    size_t Matrix::getCols() const{
+    size_t Matrix::getCols() const {
         return cols;
     }
 
     double Matrix::operator()(size_t i, size_t j) const {
-        if (i * j >= cols * rows) {throw OutOfRange(i, j, *this); }
+        if (i * j >= cols * rows) { throw OutOfRange(i, j, *this); }
         return matrix[i][j];
     }
 
-    double& Matrix::operator()(size_t i, size_t j) {
-        if (i * j >= cols * rows) {throw OutOfRange(i, j, *this); }
+    double &Matrix::operator()(size_t i, size_t j) {
+        if (i * j >= cols * rows) { throw OutOfRange(i, j, *this); }
         return matrix[i][j];
     }
 
-    bool Matrix::operator==(const Matrix& rhs) const {
+    bool Matrix::operator==(const Matrix &rhs) const {
         if (rows != rhs.rows || cols != rhs.cols) { return false; }
         bool flag = true;
         for (size_t i = 0, j = 0; i < rows || j < rhs.rows; ++i, ++j) {
@@ -57,25 +57,25 @@ namespace prep {
         return flag;
     }
 
-    bool Matrix::operator!=(const Matrix& rhs) const {
+    bool Matrix::operator!=(const Matrix &rhs) const {
         return !operator==(rhs);
     }
 
-    std::ostream& operator<<(std::ostream& os, const Matrix& matrix) {
+    std::ostream &operator<<(std::ostream &os, const Matrix &matrix) {
         os << std::setprecision(std::numeric_limits<double>::max_digits10) << matrix.rows << matrix.cols;
-        for (auto row : matrix.matrix) {
-            for (auto col : row) {
+        for (auto row: matrix.matrix) {
+            for (auto col: row) {
                 os << col;
             }
         }
-        if (os.good()) {throw InvalidMatrixStream();}
+        if (os.good()) { throw InvalidMatrixStream(); }
         return os;
     }
 
-    Matrix Matrix::operator+(const Matrix& rhs) const {
+    Matrix Matrix::operator+(const Matrix &rhs) const {
         if (rows != rhs.rows || cols != rhs.cols) { throw DimensionMismatch(*this, rhs); }
         Matrix sum(rows, cols);
-        for (size_t i = 0; i < rows ; ++i) {
+        for (size_t i = 0; i < rows; ++i) {
             for (size_t j = 0; j < cols; ++j) {
                 sum.matrix[i][j] = matrix[i][j] + rhs.matrix[i][j];
             }
@@ -83,10 +83,10 @@ namespace prep {
         return sum;
     }
 
-    Matrix Matrix::operator-(const Matrix& rhs) const {
+    Matrix Matrix::operator-(const Matrix &rhs) const {
         if (rows != rhs.rows || cols != rhs.cols) { throw DimensionMismatch(*this, rhs); }
         Matrix sum(rows, cols);
-        for (size_t i = 0; i < rows ; ++i) {
+        for (size_t i = 0; i < rows; ++i) {
             for (size_t j = 0; j < cols; ++j) {
                 sum.matrix[i][j] = matrix[i][j] - rhs.matrix[i][j];
             }
@@ -121,7 +121,7 @@ namespace prep {
 
     Matrix Matrix::operator*(double val) const {
         Matrix miltiplicated(rows, cols);
-        for (size_t i = 0; i < rows ; ++i) {
+        for (size_t i = 0; i < rows; ++i) {
             for (size_t j = 0; j < cols; ++j) {
                 miltiplicated.matrix[i][j] = val * matrix[i][j];
             }
@@ -129,19 +129,19 @@ namespace prep {
         return miltiplicated;
     }
 
-    Matrix operator*(double val, const Matrix& matrix) {
-        return matrix*val;
+    Matrix operator*(double val, const Matrix &matrix) {
+        return matrix * val;
     }
 
     double Matrix::det() const {
-            if (cols != rows) {throw DimensionMismatch(*this);}
-            double determinant = 0;
-            if (rows == 2) {
-                return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1];
-            }
-            if (rows == 1) {
-                return matrix[0][0];
-            }
+        if (cols != rows) { throw DimensionMismatch(*this); }
+        double determinant = 0;
+        if (rows == 2) {
+            return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1];
+        }
+        if (rows == 1) {
+            return matrix[0][0];
+        }
         for (size_t j = 0; j < cols; ++j) {
             Matrix minor(rows - 1, cols - 1);
             for (size_t k = 1; k < rows; ++k) {
@@ -149,7 +149,7 @@ namespace prep {
                 for (size_t l = 0; l < cols; ++l) {
                     if (l == j)
                         continue;
-                    minor.matrix[k-1][m++] = matrix[k][l];
+                    minor.matrix[k - 1][m++] = matrix[k][l];
                 }
             }
             if (j % 2 == 0) {
@@ -162,7 +162,7 @@ namespace prep {
     }
 
     Matrix Matrix::adj() const {
-        if (cols != rows) {throw DimensionMismatch(*this);}
+        if (cols != rows) { throw DimensionMismatch(*this); }
         Matrix adjugated(rows, cols);
         if (rows == 1) {
             adjugated.matrix[0][0] = 1;
@@ -171,7 +171,7 @@ namespace prep {
         Matrix transposed = transp();
         for (size_t i = 0; i < adjugated.rows; ++i) {
             for (size_t j = 0; j < adjugated.cols; ++j) {
-                Matrix minor(transposed.rows - 1,transposed.cols - 1);
+                Matrix minor(transposed.rows - 1, transposed.cols - 1);
                 size_t m = 0;
                 for (size_t k = 0; k < transposed.rows; ++k) {
                     size_t n = 0;
@@ -185,13 +185,15 @@ namespace prep {
                         ++m;
                     }
                 }
-                adjugated.matrix[i][j] = pow(-1, i + j)*minor.det();
+                adjugated.matrix[i][j] = pow(-1, i + j) * minor.det();
             }
         }
         return adjugated;
     }
 
     Matrix Matrix::inv() const {
-        return adj() * (1/det());
+        double determinant = det();
+        if (determinant == 0) { throw SingularMatrix(); }
+        return adj() * (1 / determinant);
     }
 }
